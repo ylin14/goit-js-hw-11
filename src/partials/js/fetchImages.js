6a1ba6d87229ef68ac796bf87d590cd3ee630644
ylin14 <y.linyvyi@gmail.com>
2023-05-25 18:23:57 +0300

    fixed async/await problem

--- src/partials/js/fetchImages.js
+++ src/partials/js/fetchImages.js
@@ -9,4 +9,5 @@ async function fetchImages(name, pageNumber) {
   }
 }
 
+
 export {fetchImages, perPage}
--- src/script.js
+++ src/script.js
@@ -9,7 +9,7 @@ let input = "";
 refs.form.addEventListener('submit', onSubmit);
 refs.loadMoreBtn.addEventListener('click', onLoadMoreClick)
 
-function onSubmit(event) {
+async function onSubmit(event) {
   event.preventDefault();
   refs.gallery.innerHTML = '';
   input = refs.form.elements.searchQuery.value.trim();
@@ -20,42 +20,42 @@ function onSubmit(event) {
 
   pageNumber = 1;
 
-  fetchImages(input, pageNumber)
-    .then(img => {
-     if (img.data.total === 0) {
+  try {
+    const img = await fetchImages(input, pageNumber)
+    if (img.data.total === 0) {
       Notify.failure("Sorry, there are no images matching your search query. Please try again.");
     } else {
       createGalleryMarkup(img);
     }
-     if (img.data.total > perPage) {
+    if (img.data.total > perPage) {
       pageNumber = 2;
       refs.loadMoreBtn.style.display = "block";
     }
-  })
-    .catch(error => {
+  } catch (error) {
     console.log(error);
-  });
+  }
 }
 
-function onLoadMoreClick(event) {
-
+async function onLoadMoreClick(event) {
   input = refs.form.elements.searchQuery.value.trim();
 
-  fetchImages(input, pageNumber)
-    .then(img => {
-      console.log(pageNumber);
-      const numberOfPage = Math.ceil(img.data.total / perPage);
-      if (numberOfPage > pageNumber) {
-        pageNumber += 1;
-      } else if (numberOfPage >= pageNumber) {
-        pageNumber = 1;
-        Notify.failure("We're sorry, but you've reached the end of search results.");
-        refs.loadMoreBtn.style.display = "none";
-      }
-      createGalleryMarkup(img);
-    })
-    .catch(console.log);
+  try {
+    const img = await fetchImages(input, pageNumber)
+    const numberOfPage = Math.ceil(img.data.total / perPage);
+
+    if (numberOfPage > pageNumber) {
+      pageNumber += 1;
+    } else if (numberOfPage >= pageNumber) {
+      pageNumber = 1;
+      Notify.failure("We're sorry, but you've reached the end of search results.");
+      refs.loadMoreBtn.style.display = "none";
+    }
+    createGalleryMarkup(img);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 
 
 
